decbe1260688ccd16bd389776459aed459d30a49
Waleed khan <151666902+waleedkhan69@users.noreply.github.com>
2025-05-15 09:11:03 +0500

    DSA Problem Soloving

---
+++ BasicConcept.js
@@ -0,0 +1,115 @@
+// 1
+// sum of integer
+// let a = 10;
+// let b = "20";wal
+// console.log("20" + c);
+// console.log(typeof (c));
+// 10201029
+// string
+// 2
+// Relation between integer and string
+// let a = 6;
+// let b = 7;
+// console.log("the sum of 6 and 7 ", (b + a));
+// the sum of 6 and 7  13
+// let a = 6;
+// let b = 7;
+// console.log(b + a + " isthe sum of 6 and 7 ");
+// 13 is the sum of 6 and 7
+// Q No 3
+// sum and Message
+// a section Type coercion
+// console.log(1 + "1"); = 11
+// console.log(1-"1"); = 0
+// console.log(1 * "1"); = 1
+// console.log(1 / "1"); = 1
+//  Q No 4
+//  accept and print the answer
+// let age = Number(prompt("enter your age now"))
+// console.log(typeof age);
+
+// Q No5
+// Swaping
+
+// let a = 20
+// let b = 10
+// [a, b] = [b, a]
+// console.log(a, b); //NOt working
+
+// *,+,-,/ %
+// let a = 13
+// let b = 4
+// console.log(Math.floor(a / b));
+// let a = 123123
+// console.log(a % 10);
+// 12312 ans
+// let a = 4567
+// console.log(Math.floor(a / 10));
+
+// Arithmatic Operater
+// = > > != == ===
+// console.log(10 >= 1);
+
+// Logical Operater
+// && ||
+// and mn 2no condition true hon tb true hoga agr ak b false hoi to wo false dedega
+// Or || mn agr ak b true hogi tb true dedega
+// console.log(10 > 4 && 4 > 3); // true
+// console.log(12 > 13 || 10 > 1); // false
+
+// Unaray Operater
+
+// let a = 12 // post Increament
+// let b = a++
+// console.log(a);
+// console.log(b);
+
+// let b = 10; // pre increament
+// let c = ++b
+// console.log(b);
+// console.log(c);
+// let a = 12 // post decreament
+// let b = a--
+// console.log(a);
+// console.log(b);
+// let a = 12 // pre decreament
+// let b = --a
+// console.log(a);
+// console.log(b);
+
+// let x = 11;
+// x = x++ + ++x
+// console.log(x)
+//  24 ans;
+// let a = 11; let b = 22;
+// let c = a + b + a++ + b++ + ++a + ++b
+
+
+// console.log("a=", a);
+// console.log("b=", b);
+// console.log("c=", c);
+// let a = true
+// ++a
+// console.log(a);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

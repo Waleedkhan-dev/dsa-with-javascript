6003d58158f8d0f0439de134255e51a2b5930beb
Waleed khan <151666902+waleedkhan69@users.noreply.github.com>
2025-05-16 12:00:42 +0500

    Electricity Bils

---
+++ conditionalState.js
@@ -0,0 +1,55 @@
+// Conditional Statement
+
+// Vote Validation  Q No 1
+// let voter = Number(prompt("Enter voter age"))
+// if (isNaN(voter)) {
+//  console.log("Please Enter the correct Value");
+// }
+// else if (voter >= 18) {
+//  console.log("you are eligible for vote");
+// } else {
+//  console.log("you are not eligible for vote");
+// }
+
+// Question No 2
+// shope Discount  payable amount show krwani ha
+// const amount = Number(prompt("Enter the Amout"))
+// let dis = 0
+// if (amount > 0 && amount <= 5000) {
+//  dis = 0
+// } else if (amount > 5000 && amount <= 7000) {
+//  dis = 5
+// } else if (amount > 7000 && amount <= 9000) {
+//  dis = 10
+// }
+// else if (amount > 9000 ) {
+//  dis = 20
+// }
+
+// else {
+//  console.log("please enter the valid amount");
+// }
+// console.log(Math.floor(amount - (dis * amount) / 100))
+
+// Question NO 3
+// calculate Electricity Bils 
+
+let unit = prompt("Enter your electricity Unit")
+console.log("hello");
+
+
+let amout = 0;
+if (unit > 400) {
+ amout = (unit - 400) * 13
+ unit = 400
+}
+if (unit > 200 && unit <= 400) {
+ amout += (unit - 200) * 8
+ unit = 200
+}
+if (unit > 100 && unit >= 200) {
+ amout += (unit - 100) * 6
+ unit = 100
+}
+amout += unit * 4
+console.log(amout); 

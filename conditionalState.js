daf7aa936d426d6f6eb5b03b6829e9963c8450c2
Waleed khan <151666902+waleedkhan69@users.noreply.github.com>
2025-05-18 12:33:23 +0500

    Electricity Bills

--- conditionalState.js
+++ conditionalState.js
@@ -32,24 +32,37 @@
 // console.log(Math.floor(amount - (dis * amount) / 100))
 
 // Question NO 3
-// calculate Electricity Bils 
+// calculate Electricity Bils
 
-let unit = prompt("Enter your electricity Unit")
-console.log("hello");
+// let unit = prompt("Enter your electricity Unit")
+// console.log("hello")
 
 
-let amout = 0;
-if (unit > 400) {
- amout = (unit - 400) * 13
- unit = 400
-}
-if (unit > 200 && unit <= 400) {
- amout += (unit - 200) * 8
- unit = 200
-}
-if (unit > 100 && unit >= 200) {
- amout += (unit - 100) * 6
- unit = 100
-}
-amout += unit * 4
-console.log(amout); 
+// let amout = 0;
+// if (unit > 400) {
+//  amout = (unit - 400) * 13
+//  unit = 400
+// }
+// if (unit > 200 && unit <= 400) {
+//  amout += (unit - 200) * 8
+//  unit = 200
+// }
+// if (unit > 100 && unit >= 200) {
+//  amout += (unit - 100) * 6
+//  unit = 100
+// }
+// amout += unit * 4
+// console.log(amout);
+// let unit = Number(prompt("Enter your Unit "))
+// let amount = 0
+// if (unit > 400) {
+//  amount = (unit - 400) * 13
+// }
+// if (unit > 200 && unit <= 400) {
+//  amount += (unit - 200) * 8
+// }
+// if (unit > 100 && unit <= 200) {
+//  amount += (unit - 100) * 6
+// }
+// amount += unit *4
+

43a1792b5a54d62c015392ac830559311dea72bc
Waleed khan <151666902+waleedkhan69@users.noreply.github.com>
2025-05-19 12:43:06 +0500

    switch statment , Nested Ternary operater ,Ternary operater

--- conditionalState.js
+++ conditionalState.js
@@ -34,35 +34,137 @@
 // Question NO 3
 // calculate Electricity Bils
 
-// let unit = prompt("Enter your electricity Unit")
-// console.log("hello")
-
-
-// let amout = 0;
-// if (unit > 400) {
-//  amout = (unit - 400) * 13
-//  unit = 400
-// }
-// if (unit > 200 && unit <= 400) {
-//  amout += (unit - 200) * 8
-//  unit = 200
-// }
-// if (unit > 100 && unit >= 200) {
-//  amout += (unit - 100) * 6
-//  unit = 100
-// }
-// amout += unit * 4
-// console.log(amout);
-// let unit = Number(prompt("Enter your Unit "))
+// let unit = Number(prompt("Enter your electricity unit"))
 // let amount = 0
 // if (unit > 400) {
 //  amount = (unit - 400) * 13
+//  amount = 400
 // }
 // if (unit > 200 && unit <= 400) {
 //  amount += (unit - 200) * 8
+//  unit = 200
 // }
 // if (unit > 100 && unit <= 200) {
 //  amount += (unit - 100) * 6
+//  unit = 100
 // }
-// amount += unit *4
+// amount += unit * 4
+// console.log(amount);
+
+
+// Question NO 3
+// pkr denomination
+
+// let amount = 595505
+
+// if (amount >= 5000) {
+//  console.log("5k notes  " + Math.floor(amount / 5000));
+//  amount = amount % 5000
+
+// }
+// if (amount >= 1000) {
+//  console.log("1k notes  " + Math.floor(amount / 1000));
+//  amount = amount % 1000
+// }
+// if (amount >= 500) {
+//  console.log("500 notes  " + Math.floor(amount / 500));
+//  amount = amount % 500
+
+// }
+// if (amount >= 100) {
+//  console.log("50 notes: " + Math.floor(amount / 100));
+//  amount = amount % 100
+
+// }
+// if (amount >= 50) {
+//  console.log("50 notes: " + Math.floor(amount / 50));
+//  amount = amount % 50
+
+// }
+// if (amount >= 20) {
+//  console.log("20 notes: " + Math.floor(amount / 20));
+//  amount = amount % 20
+
+// }
+// if (amount >= 10) {
+//  console.log("10 notes " + Math.floor(amount / 10));
+//  amount = amount % 10
+// }
+// if (amount >= 2) {
+//  console.log("5 notes" + Math.floor(amount / 5));
+//  amount = amount % 5
+
+// }
+// if (amount >= 2) {
+//  console.log("2 coins " + Math.floor(amount / 2));
+//  amount = amount % 2
+// }
+// if (amount === 1) {
+//  console.log("1 coins :", amount);
+
+// }
+
+// Topics
+// ternary OPerater  ? and : are ternary operater
+// 12 > 111 ? console.log("waleed") : console.log("saqlain");
+// console.log(12 > 13 ? "waleed" : "saqlain");
+// Nested Ternary operater
+
+// let num = 92
+
+// console.log(num > 12 ? "plus" : num < 12 ? "minus" : "nuteral");
+
+// switch Statment
+// 1 mothed
+// let num = Number(prompt("Enter Date Please"))
+
+// switch (num) {
+//  case 23: console.log("finish salary");
+//   break
+//  case 1: console.log("salary Day")
+//   break
+//  case 25: console.log("finsih Salary");
+//   break
+
+//  default: console.log("please weak up");
+
+// }
+// 2 method
+// let num = 3
+// switch (num) {
+//  case 1:
+//  case 2:
+//  case 3:
+//  case 4: console.log("Thrisday");
+//   break
+//  default: console.log("Please  enter the correct Value");
+
+// }
+// 3 method
+
+// switch (true) {
+//  case 12 > 3: console.log("i  am waleed");
+//   break
+//  case 10 < 6: console.log("you are wrong");
+//   break
+//  default: console.log(" acha hoa sahi ni write kea haha ");
+
+
+// }
+
+// let a = (0.2 + 0.1)// chatgpt
+// switch (a.toFixed(1)) {
+//  case (0.3).toFixed(1): console.log("true");
+//   break
+//  case 0.5: console.log("false");
+//   break
+//  default: console.log("Heloo coder");
+// }
+
+
+
+
+
+
+
 

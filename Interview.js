6268a55600f2c81fce79a066d6e7cd4b2c0af736
Waleed khan <151666902+waleedkhan69@users.noreply.github.com>
2025-06-03 22:54:47 +0500

    Repeat Hello

---
+++ Interview.js
@@ -0,0 +1,66 @@
+// var Arr = [1, 2, 3, 4, 5, 1, 22, 2, 2]
+// var newArry = new Set(Arr)
+// console.log(newArry);
+
+// function Reverse(str) {
+//  return str.split("").reverse().join("")
+// }
+// console.log(Reverse("waleed"));
+// function isprime(Str) {
+//  if (Str <= 1) return false
+//  for (let i = 2; i <= Math.sqrt(Str); i++) {
+//   if (Str % i === 0) return false
+//  }
+//  return true
+// }
+// console.log(isprime(12));
+
+// let arr = [1, 4, 6, 8]
+// let sum = arr.reduce((a, b) => {
+//  return a + b
+// })
+// console.log(sum);
+
+// revese Without using revers
+// let arr = [1, 3, 4]
+// let reverse = []
+// for (let i = arr.length - 1; i >= 0; i--) {
+//  reverse.push(arr[i])
+// }
+// console.log(reverse);
+// let arr = [1, 23]
+// let newArry = []
+// for (let i = arr.length - 1; i >= 0; i--) {
+//  newArry.push(arr[i])
+// }
+// console.log(newArry);
+
+// remove Duplicate Value
+
+// let e = [1, 22, 3, 3, 33, 33]
+// let b = [...new Set(e)]
+// console.log(b);
+
+
+// let objAry = [
+//  { name: "waleed", age: 20 },
+//  { name: "saqlain", age: 23 }
+// ]
+// let result = {}
+// objAry.forEach(user => {
+//  result[user.id] = user.name
+// })
+
+
+
+
+
+
+
+
+
+
+
+
+
+

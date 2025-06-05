bed50703f115d445b6a076f8aa051052a4ee7d98
Waleed khan <151666902+waleedkhan69@users.noreply.github.com>
2025-06-05 21:45:07 +0500

    Pattrens Programming

--- PattrensProgramming.js
+++ PattrensProgramming.js
@@ -1,7 +1,7 @@
 // If i wanna the print in the same line of multipul log the we use this mathod
 // process.stdout.write("hello  ")
 // process.stdout.write("waleed")
-let num = 6
-for (let i = 1; i <= num; i++) {
- process.stdout.write("* ")
-}
+// let num = 6
+// for (let i = 1; i <= num; i++) {
+//  process.stdout.write("* ")
+// }

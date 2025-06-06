8628d1e60975165fe0c7a784f0afd40b3079cf76
Waleed khan <151666902+waleedkhan69@users.noreply.github.com>
2025-06-06 15:39:29 +0500

    nested Looping & terminal output

--- PattrensProgramming.js
+++ PattrensProgramming.js
@@ -5,3 +5,35 @@
 // for (let i = 1; i <= num; i++) {
 //  process.stdout.write("* ")
 // }
+
+// simple Way
+// let numStar = 5
+// for (let i = 1; i <= numStar; i++) {
+//  process.stdout.write("* ")
+// }
+// console.log();
+
+// for (let i = 1; i <= numStar; i++) {
+//  process.stdout.write("* ")
+// }
+// console.log();
+
+// for (let i = 1; i <= numStar; i++) {
+//  process.stdout.write("* ")
+// }
+// console.log();
+
+// for (let i = 1; i <= numStar; i++) {
+//  process.stdout.write("* ")
+// }
+
+// base Pattrens
+// let n = 6
+// for (let i = 1; i <= n; i++) {
+//  for (let j = 1; j <= n; j++) {
+//   process.stdout.write("* ")
+//  }
+//  console.log();
+// }
+
+// Pattrens Programming

fa61e996873964ff03a5fc8ae34249df8a42b980
Waleed khan <151666902+waleedkhan69@users.noreply.github.com>
2025-06-07 08:40:25 +0500

    Arrey Method

---
+++ Arey.js
@@ -0,0 +1,50 @@
+// let arr = []
+// arr.push(12)
+// arr.push(false)
+// arr.push("waleed")
+// console.log(arr);
+
+// arr.pop()
+// console.log(arr);
+// let Arey = []
+// Arey[0] = 123
+// Arey[1] = 22
+// Arey[5] = "waled"
+// console.log(Arey);
+// let ar = new Array(3)
+// ar[1] = 10
+// ar[2] = 20
+// ar[3] = 3
+// console.log(ar);
+
+// let arr = new Array(3)
+// arr.push(12)
+// console.log(arr); //(4) [empty × 3, 12]
+
+// geting Data from Prompt
+// let arry = new Array(5)
+// for (let i = 0; i < arry.length; i++) {
+//  arry[i] = Number(prompt("Ente rthe value"))
+// }
+// console.log(arry);
+
+// Sum of arry
+// let arr = [10, 20, 10, 10]
+// let sum = 0
+// for (let i = 0; i < arr.length; i++) {
+//  sum = sum + arr[i]
+// }
+// console.log(sum);
+// Question 2 Max Element In the arrey 
+let a = [1, 2, 3, 4]
+let mx = a[i]
+
+
+for (let i = 0; i < a.length; i++) {
+ if (mx < a[i]) {
+  mx = a[i]
+ }
+}
+
+
+
--- PattrensProgramming.js
+++ PattrensProgramming.js
@@ -46,11 +46,81 @@
 //  }
 //  console.log();
 // }
-let n = 20
-for (let i = 1; i <= n; i++) {
- for (let j = 1; j <= i; j++) {
-  process.stdout.write(`${j} `)
- }
- console.log();
+// let n = 20
+// for (let i = 1; i <= n; i++) {
+//  for (let j = 1; j <= i; j++) {
+//   process.stdout.write(j + " ")
+//  }
+//  console.log();
+// }
 
-}
+// Right Angle With Charactur
+// let CHar = 16
+// for (let i = 1; i <= CHar; i++) {
+//  let asscii = 65
+//  for (let j = 1; j <= i; j++) {
+//   process.stdout.write(String.fromCharCode(asscii + "   "))
+//   asscii++
+//  }
+//  console.log();
+// }
+// inverted Right Angle Triangle
+// let n = 5
+// for (let i = 1; i <= n; i++) {
+//  for (let j = 1; j <= n - i + 1; j++) {
+//   process.stdout.write("* ")
+//  }
+//  console.log();
+// }
+// second Method
+
+// let n = 5
+// for (let i = 1; i <= n; i++) {
+//  for (let j = n; j >= i; j--) {
+//   process.stdout.write("* ")
+//  }
+//  console.log();
+// }
+// mirror angle  Triangle
+// let n = 5
+// for (let i = 1; i <= n; i++) {
+//  for (let j = 1; j <= n - i; j++) {
+//   process.stdout.write("  ")
+//  }
+//  for (let j = 1; j <= i; j++) {
+//   process.stdout.write("* ")
+//  }
+//  for (let j = 1; j <= n - i; j++) {
+//   process.stdout.write("  ")
+//  }
+//  console.log();
+
+// }
+
+// X pattrens
+// let n = 5
+// for (let i = 1; i <= n; i++) {
+//  for (let j = 1; j <= n; j++) {
+//   if (i === j || i + j == n + 1) {
+//    process.stdout.write("* ")
+//   } else {
+//    process.stdout.write(" ")
+//   }
+//  }
+//  console.log();
+
+// }
+
+// V type pattrens In PP
+// let n = 5
+// for (let i = 1; i <= n; i++) {
+//  for (let j = 1; j <= 2 * n; j++) {
+//   if (i === j || i + j == 2 * n) {
+//    process.stdout.write(" *")
+//   }
+//   else {
+//    process.stdout.write(" ")
+//   }
+//  }
+//  console.log();
+// }
